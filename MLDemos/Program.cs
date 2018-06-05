939c6b7cf81a7630250ea61a6f24a6791ddf0308
Bas <basnetten@gmail.com>
2018-06-05 18:40:32 +0200

    Extract 2x2 NeuralNet to class
    The simple 2x2 neural network is available as a class. This allows for
    easier usage of the neural network.
    
    Add region around the extension functions.

--- MLDemos/Program.cs
+++ MLDemos/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using DataStructures.Matrices;
 using DataStructures.Vectors;
 
@@ -18,6 +19,22 @@ namespace MLDemos
 			Vector v2  = w1.Mul(input);
 			Vector v2o = v2.ApplySigmoid();
 			Console.WriteLine($"2x2 out: {v2o}");
+
+			NNet nn2x2 = new NNet(1)
+			{
+				Input = Vector.FromArray(new[] { 1.0, 0.5 }),
+				Weights = new[]
+				{
+					new Matrix().FromArray(new double[,]
+					{
+						{ 0.9, 0.2 },
+						{ 0.3, 0.8 },
+					}),
+				}
+			};
+
+			nn2x2.FeedForward();
+			Console.WriteLine($"nn2x2 out: {nn2x2.Output}");
 
 			// 3 x 3 NN
 			Vector in3x3 = Vector.FromArray(new[] { 0.9, 0.2, 0.8 });
@@ -40,6 +57,36 @@ namespace MLDemos
 			Vector o3x3_2  = in3x3_2.ApplySigmoid();
 			Console.WriteLine($"3x3 out: {o3x3_2}");
 		}
+
+		public class NNet
+		{
+			public NNet(int hiddenLayerCount)
+			{
+				HiddenLayerCount = hiddenLayerCount;
+				HiddenLayers = new Vector[hiddenLayerCount + 1];
+			}
+
+			public Vector Input
+			{
+				set => HiddenLayers[0] = value;
+			}
+
+			public int HiddenLayerCount { get; set; }
+			public Matrix[] Weights      { get; set; }
+			public Vector[] HiddenLayers { get; }
+
+			public Vector Output => HiddenLayers.Last();
+
+			public void FeedForward()
+			{
+				for (int i = 0; i < HiddenLayerCount; i++)
+				{
+					HiddenLayers[i + 1] = Weights[i].Mul(HiddenLayers[i]).ApplySigmoid();
+				}
+			}
+		}
+
+		#region Extensions
 
 		// TODO Use DataStructures implementation once released.
 		private static Vector Mul(this Matrix m, Vector v)
@@ -89,5 +136,7 @@ namespace MLDemos
 		{
 			return 1d / (1 + Math.Pow(Math.E, -d));
 		}
+
+		#endregion
 	}
 }
