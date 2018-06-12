5c9fbeaf479eaebca160da6a47d92e4ae1533ff6
Bas <basnetten@gmail.com>
2018-06-12 22:08:11 +0200

    Add backpropogation multiple layers

--- MLDemos/Program.cs
+++ MLDemos/Program.cs
@@ -9,42 +9,42 @@ namespace MLDemos
 	{
 		public static void Main(string[] args)
 		{
-			NNet nn2X2 = new NNet(1)
-			{
-				Input = Vector.FromArray(new[] { 1.0, 0.5 }),
-				Weights = new[]
-				{
-					new Matrix().FromArray(new[,]
-					{
-						{ 0.9, 0.2 },
-						{ 0.3, 0.8 },
-					}),
-				}
-			};
-			nn2X2.FeedForward();
-			Console.WriteLine($"nn2x2 out: {nn2X2.Output}");
+//			NNet nn2X2 = new NNet(0)
+//			{
+//				Input = Vector.FromArray(new[] { 1.0, 0.5 }),
+//				Weights = new[]
+//				{
+//					new Matrix().FromArray(new[,]
+//					{
+//						{ 0.9, 0.2 },
+//						{ 0.3, 0.8 },
+//					}),
+//				}
+//			};
+//			nn2X2.FeedForward();
+//			Console.WriteLine($"nn2x2 out: {nn2X2.Output}");
 
-			NNet nn3X3 = new NNet(2)
-			{
-				Input = Vector.FromArray(new[] { 0.9, 0.2, 0.8 }),
-				Weights = new[]
-				{
-					new Matrix().FromArray(new[,]
-					{
-						{ 0.9, 0.3, 0.4 },
-						{ 0.2, 0.8, 0.2 },
-						{ 0.1, 0.5, 0.6 },
-					}),
-					new Matrix().FromArray(new[,]
-					{
-						{ 0.3, 0.7, 0.5 },
-						{ 0.6, 0.5, 0.2 },
-						{ 0.8, 0.1, 0.9 },
-					})
-				}
-			};
-			nn3X3.FeedForward();
-			Console.WriteLine($"nn3x3 out: {nn3X3.Output}");
+//			NNet nn3X3 = new NNet(1)
+//			{
+//				Input = Vector.FromArray(new[] { 0.9, 0.2, 0.8 }),
+//				Weights = new[]
+//				{
+//					new Matrix().FromArray(new[,]
+//					{
+//						{ 0.9, 0.3, 0.4 },
+//						{ 0.2, 0.8, 0.2 },
+//						{ 0.1, 0.5, 0.6 },
+//					}),
+//					new Matrix().FromArray(new[,]
+//					{
+//						{ 0.3, 0.7, 0.5 },
+//						{ 0.6, 0.5, 0.2 },
+//						{ 0.8, 0.1, 0.9 },
+//					})
+//				}
+//			};
+//			nn3X3.FeedForward();
+//			Console.WriteLine($"nn3x3 out: {nn3X3.Output}");
 
 			NNet xor = new NNet(1)
 			{
@@ -54,13 +54,21 @@ namespace MLDemos
 					new Matrix().FromArray(new[,]
 					{
 						{ 0.9, 0.2 },
+						{ 0.3, 0.8 },
+					}),
+					new Matrix().FromArray(new[,]
+					{
+						{ 0.7, 0.6 },
 						//{ 0.3 }, //, 0.8 },
 					}),
 				}
 			};
 			xor.FeedForward();
 			Console.WriteLine($"XOR: {xor.Output}");
+			Console.WriteLine(xor);
 			xor.BackPropogate(new[] { 0.1 }); // Expect 0 (becomes 0.1).
+			Console.WriteLine(xor);
+
 			xor.FeedForward();
 			Console.WriteLine($"XOR: {xor.Output}");
 			for (int i = 0; i < 100000; i++)
@@ -68,6 +76,7 @@ namespace MLDemos
 				xor.FeedForward();
 				xor.BackPropogate(new[] { 0.9 }); // Expect 0 (becomes 0.1).
 			}
+
 			xor.FeedForward();
 			Console.WriteLine($"XOR: {xor.Output}");
 		}
@@ -77,7 +86,7 @@ namespace MLDemos
 			public NNet(int hiddenLayerCount)
 			{
 				HiddenLayerCount = hiddenLayerCount;
-				HiddenLayers     = new Vector[hiddenLayerCount + 1];
+				HiddenLayers     = new Vector[hiddenLayerCount + 2];
 			}
 
 			public Vector Input
@@ -95,49 +104,62 @@ namespace MLDemos
 
 			public void FeedForward()
 			{
-				for (int i = 0; i < HiddenLayerCount; i++)
+				for (int i = 0; i < HiddenLayerCount + 1; i++)
 				{
-					HiddenLayers[i + 1] = ((Vector) Weights[i].Mul((Matrix) HiddenLayers[i])).ApplySigmoid();
+					HiddenLayers[i + 1] = (Weights[i] * HiddenLayers[i]).ApplySigmoid();
 				}
 			}
 
 			public void BackPropogate(double[] expectedOutput)
 			{
-				double[] expected = expectedOutput;
-				for (int currentLayer = HiddenLayerCount; currentLayer >= 1; currentLayer--)
+				// double[] expected = expectedOutput;
+
+				double[] errorArray = new double[expectedOutput.Length];
+				for (int i = 0; i < Output.Count; i++)
+					errorArray[i] = expectedOutput[i] - Output[i];
+
+				for (int currentLayer = HiddenLayerCount + 1; currentLayer >= 1; currentLayer--)
 				{
 					// Retrieve weights for layer.
 					IMatrix   w  = Weights[currentLayer - 1];
 					double[,] dw = new double[w.RowCount, w.ColCount];
+
+					double[] nextErrorArray = new double[HiddenLayers[currentLayer - 1].Count];
 
 					for (int destNodeI = 0; destNodeI < HiddenLayers[currentLayer].Count; destNodeI++)
 					{
 						double valCurrentDest  = HiddenLayers[currentLayer][destNodeI];
 						Vector valsSourceNodes = HiddenLayers[currentLayer - 1];
 
-						double error = expected[destNodeI] - valCurrentDest;
-
 						double sigma = 0d;
 						for (int sourceNodeI = 0; sourceNodeI < valsSourceNodes.Count; sourceNodeI++)
 						{
 							// Get the weight for this set.
 							sigma += valsSourceNodes[sourceNodeI] * w[destNodeI, sourceNodeI];
+
+							nextErrorArray[sourceNodeI] += errorArray[destNodeI] * w[destNodeI, sourceNodeI];
 						}
 
 						double sigmoid = Sigmoid(sigma);
 
-						double total = -error * sigmoid * (1 - sigmoid) * HiddenLayers[currentLayer][destNodeI];
+						double total = -errorArray[destNodeI] * sigmoid * (1 - sigmoid) *
+						               HiddenLayers[currentLayer][destNodeI];
 
 						for (int sourceNodeI = 0; sourceNodeI < valsSourceNodes.Count; sourceNodeI++)
 						{
 							dw[destNodeI, sourceNodeI] += -(total * w[destNodeI, sourceNodeI] * 0.1); // TODO learnrate
 						}
-
-//						Console.WriteLine(total);
 					}
+
+					errorArray = nextErrorArray;
 
 					Weights[currentLayer - 1] += new Matrix().FromArray(dw);
 				}
+			}
+
+			public override string ToString()
+			{
+				return $"{string.Join<Vector>(Environment.NewLine, HiddenLayers)}";
 			}
 		}
 
